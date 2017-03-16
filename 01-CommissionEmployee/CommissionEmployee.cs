a7b697191d0c29439209c7d2cf2076a40fbbec0b
Clase <Clase>
2017-03-15 19:35:11 -0500

    Tercera Semana

---
+++ 01-CommissionEmployee/CommissionEmployee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class CommissionEmployee : object
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set;  }
+    public string SocialSecurityNumber { get; set;  }
+   private decimal grossSales; // Ventas brutas semanales
+   private decimal commissionRate; // porcentaje de comision
+
+   public CommissionEmployee(string firstName, string lastName,
+      string socialSecurityNumber, decimal grossSales, decimal commissionRate)
+   {
+      FirstName = firstName;
+      LastName = lastName;
+      SocialSecurityNumber = socialSecurityNumber;
+      GrossSales = grossSales; // propiedad que valida las ventas brutas semanales
+      CommissionRate = commissionRate; // propiedad que valida el porcentaje de comision
+   }
+
+   public decimal GrossSales
+   {
+      get
+      {
+         return grossSales;
+      }
+      set
+      {
+         if (value < 0) 
+         {
+            throw new ArgumentOutOfRangeException(value.ToString(), value, "{nameof(GrossSales)} must be >= 0");
+         }
+
+         grossSales = value;
+      }
+   }
+
+   public decimal CommissionRate
+   {
+      get
+      {
+         return commissionRate;
+      }
+      set
+      {
+         if (value <= 0 || value >= 1)
+         {
+            throw new ArgumentOutOfRangeException(value.ToString(), value, "{nameof(CommissionRate)} must be > 0 and < 1");
+         }
+
+         commissionRate = value;
+      }
+   }
+
+   //public decimal Earnings() => commissionRate * grossSales;
+    public decimal Earnings()
+    {
+        return commissionRate * grossSales;
+    }
+
+    public override string ToString()
+    {
+        return  "Empleado por comision   : " + FirstName + " " + LastName+"\n"+
+                "Numero de Seguro Social : " + SocialSecurityNumber+"\n" +
+                "Ventas Brutas           : " + grossSales+"\n" +
+                "Porcentaje de Comision  : " + commissionRate;
+    }
+}
---
+++ 01-CommissionEmployee/CommissionEmployeeTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class CommissionEmployeeTest
+{
+   static void Main()
+   {
+		Console.WriteLine("(01) Empleado por Comision\n");
+
+		var employee = new CommissionEmployee("Sue", "Jones",
+         "222-22-2222", 1000.00M, .06M);
+
+      Console.WriteLine("Informacion del empleado obtenida por las propiedades y metodos: \n");
+      Console.WriteLine("Nombre                  : {0}", employee.FirstName);
+      Console.WriteLine("Apellido                : {0}", employee.LastName);
+      Console.WriteLine("Numero de Seguro Social : {0}", employee.SocialSecurityNumber);
+      Console.WriteLine("Ventas Brutas           : {0}", employee.GrossSales);
+      Console.WriteLine("Porcentaje de Comision  : {0}", employee.CommissionRate);
+      Console.WriteLine("Ganancias               : {0}", employee.Earnings());
+
+      employee.GrossSales = 5000.00M; // Se actualiza las ventas brutas del empleado
+      employee.CommissionRate = .1M; // Se actualiza el porcentaje de comision del empleado
+
+      Console.WriteLine("\n"+"Informacion actualizada obtenida por :"+"\n"+employee.ToString()+"\n");
+      Console.WriteLine(employee);
+      Console.WriteLine("Ganancias               : {0}\n", employee.Earnings());
+   }
+}
